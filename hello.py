3d0c01000a9f1dbe1a5ee0816cde690fed57698f
Zdeněk Barth <zdebarth@gmail.com>
2024-06-01 11:04:12 +0200

    Extract from 100 Days of Python

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/bye")
+def say_bye():
+    return "Bye!"
+
+
+@app.route("/username/<name>")
+def greet(name):
+    return f"Hello {name}!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
